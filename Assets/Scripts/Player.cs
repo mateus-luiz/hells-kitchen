2de877f73c456daab6fccd7ffbe798119f6e5451
Sttatic <mateus.florentino2001@gmail.com>
2023-05-22 00:27:07 -0300

    Made player rotation to input direction / Create PlayerAnimator script and setup walking animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update() {
         Vector2 inputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
@@ -12,5 +13,13 @@ public class Player : MonoBehaviour
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y); 
         transform.position += moveDirection * moveSpeed * Time.deltaTime;
+        isWalking = moveDirection != Vector3.zero;
+
+        float rotationSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDirection, rotationSpeed * Time.deltaTime);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 }
