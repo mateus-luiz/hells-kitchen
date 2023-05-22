5d21ef63349eb9f4209ff314d1893ffbd2b96016
Sttatic <mateus.florentino2001@gmail.com>
2023-05-22 13:57:58 -0300

    Made player collision detect

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,35 @@ public class Player : MonoBehaviour
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
         Vector3 moveDirection = new Vector3(inputVector.x, 0f, inputVector.y); 
-        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirection, moveDistance);
+
+        if(!canMove) {
+            //Não pode se mover 
+            //Verica o movimento no eixo X
+            Vector3 moveDirectionX = new Vector3(moveDirection.x, 0, 0).normalized;
+            canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionX, moveDistance);
+            
+            if(canMove) {
+                //Pode se mover somente no eixo X
+                moveDirection = moveDirectionX;
+            } else {
+                //Não pode se mover no eixo X
+                //Verifica o eixo Z
+                Vector3 moveDirectionZ = new Vector3(0, 0, moveDirection.z).normalized;
+                canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirectionZ, moveDistance);
+
+                if(canMove) {
+                    //Pode se mover somente no eixo Z
+                    moveDirection = moveDirectionZ;
+                }
+            }
+        }
+
+        if(canMove) transform.position += moveDirection * moveDistance;
         isWalking = moveDirection != Vector3.zero;
 
         float rotationSpeed = 10f;
